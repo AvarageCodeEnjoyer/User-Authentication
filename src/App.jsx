dba4f401ac75b90f1529e5b8032e1788b9f25c35
AvarageCodeEnjoyer <ndross427@West-MEC.org>
2023-11-14 10:18:32 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
+import { Login, Signup, Home } from "./pages"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
